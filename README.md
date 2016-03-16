7f6fddff08e4f7b7a6c132df7ed5430bee214f6e
Luis Puchades <luispuchades@live.com>
2016-03-16 07:22:30 +0100

    HTML5PildorasInformaticasVideo48
    HTML5 - Curso - Píldoras Informáticas - API Web Storage I - Video48 -
    Almacenando Información

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTML5PildorasInformaticasVideo48
+HTML5 - Curso - Píldoras Informáticas - Web Storage I - Video48 - Almacenando Información
+<br />
+www.pildorasinformaticas.es
+<br />
+https://www.youtube.com/watch?v=KswlJImD9gI
+<br />
---
+++ js/funciones48.js
@@ -0,0 +1,65 @@
+/*global window */
+/*global alert */
+/*jslint browser: true, for:true */
+
+/**JavaScript Document
+ * Curso: HMTL5 - Pildoras Informáticas - Web Storage I
+ * Origin: Capitulo48.html ==> Almacenando Información
+ */
+
+// "use strict";
+
+
+//1. Definición de Objetos y Variables
+var botonGrabar;
+var itemClave;
+var itemValor;
+var zonaDatos;
+
+
+
+//1.1 Extracción de elementos desde HTML
+botonGrabar = document.getElementById("grabar");
+zonaDatos = document.getElementById("zona-datos");
+
+
+
+//2. Definición de Funciones
+
+function itemMostrar(itemClave) {
+    'use strict';
+
+    var itemValorMostrar;
+
+    itemValorMostrar = sessionStorage.getItem(itemClave);
+
+    zonaDatos.innerHTML = "<div>Clave: " + itemClave + "--" + "Valor: " + itemValorMostrar + "</div>";
+}
+
+
+function itemNuevo() {
+    'use strict';
+
+//OJO DEFINIMOS itemClave e itemValor como variables locales. Si las
+// definimos como variables generales, no funciona
+    itemClave = document.getElementById("clave").value;
+    itemValor = document.getElementById("valor").value;
+
+    sessionStorage.setItem(itemClave, itemValor);
+
+    itemMostrar(itemClave);
+}
+
+
+
+
+function comenzar() {
+    'use strict';
+
+    botonGrabar.addEventListener("click", itemNuevo, false);
+
+}
+
+
+//3. Asignación de Eventos
+document.addEventListener("DOMContentLoaded", comenzar, false);
